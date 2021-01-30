7e6fac9078f02b33dc81c331c597590e81cdb24e
Vaishnavi <vaishdw2207@gmail.com>
2021-01-31 04:21:17 +0530

    feat: add daily hacks tracker

---
+++ lib/constants/routine-list.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class RoutineList extends StatefulWidget {
+  @override
+  _RoutineListState createState() => _RoutineListState();
+}
+
+class _RoutineListState extends State<RoutineList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: ListView.builder(
+            shrinkWrap: true,
+            scrollDirection: Axis.horizontal,
+            itemCount: 5,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(context, '/');
+                  },
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.35,
+                    child: Text('this.routine'),
+                    decoration: BoxDecoration(
+                        color: Colors.orangeAccent[200].withOpacity(0.6),
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                ),
+              );
+            }));
+  }
+}
--- lib/screens/homey.dart
+++ lib/screens/homey.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:niyamo/constants/AppTheme.dart';
 import 'package:niyamo/constants/carouselz.dart';
+import 'package:niyamo/constants/routine-list.dart';
 
 class Homey extends StatefulWidget {
   @override
@@ -10,35 +13,105 @@ class _HomeyState extends State<Homey> {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Stack(
-        children: [
-          Padding(
-            padding: EdgeInsets.only(
-                top: MediaQuery.of(context).size.height * 0.04,
-                left: MediaQuery.of(context).size.width * 0.1),
-            child: Text(
-              'explore',
-              style: TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.w900,
-                  decoration: TextDecoration.overline),
-            ),
-          ),
-          Caraou(),
-          Padding(
-            padding: EdgeInsets.only(
-                top: MediaQuery.of(context).size.height * 0.3,
-                left: MediaQuery.of(context).size.width * 0.1),
-            child: Text(
-              'your routine',
-              style: TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.w900,
-                  decoration: TextDecoration.overline),
-            ),
-          ),
-        ],
+        child: Stack(children: [
+      Padding(
+        padding: EdgeInsets.only(
+            top: MediaQuery.of(context).size.height * 0.04,
+            left: MediaQuery.of(context).size.width * 0.1),
+        child: Text(
+          'explore',
+          style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.w900,
+              decoration: TextDecoration.overline),
+        ),
       ),
-    );
+      Caraou(),
+      Padding(
+        padding: EdgeInsets.only(
+            top: MediaQuery.of(context).size.height * 0.3,
+            left: MediaQuery.of(context).size.width * 0.1),
+        child: Text(
+          'your routine',
+          style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.w900,
+              decoration: TextDecoration.overline),
+        ),
+      ),
+      // Expanded(child: RoutineList())
+      Padding(
+        padding: EdgeInsets.only(
+            top: MediaQuery.of(context).size.height * 0.35,
+            right: MediaQuery.of(context).size.width * 0.05,
+            left: MediaQuery.of(context).size.width * 0.05),
+        child: Container(
+          height: 100,
+          decoration: BoxDecoration(
+              color: AppTheme.ellowPinky,
+              borderRadius: BorderRadius.circular(10)),
+          padding: EdgeInsets.all(15),
+          child: ListView.builder(
+            itemCount: 7,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (ctx, f) {
+              int day = DateTime.now().day + f;
+              return FittedBox(
+                child: Container(
+                  width: 90,
+                  height: 90,
+                  margin: EdgeInsets.only(right: 15.0),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: day == DateTime.now().day
+                        ? Colors.cyan[400]
+                        : Colors.amber[500],
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  padding: EdgeInsets.all(15.0),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          (day < 32)
+                              ? "${DateTime.now().day + f}"
+                              : (day == 32)
+                                  ? 1.toString()
+                                  : (f).toString(),
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: day == DateTime.now().day
+                                ? FontWeight.bold
+                                : FontWeight.normal,
+                            color: day == DateTime.now().day
+                                ? Colors.black
+                                : Colors.black,
+                          ),
+                        ),
+                        Text(
+                          DateFormat('EE').format(
+                            DateTime.now().add(
+                              Duration(days: f),
+                            ),
+                          ),
+                          style: TextStyle(
+                              color: day == DateTime.now().day
+                                  ? Colors.black
+                                  : Colors.black,
+                              fontWeight: day == DateTime.now().day
+                                  ? FontWeight.bold
+                                  : FontWeight.normal),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    ]));
   }
 }
