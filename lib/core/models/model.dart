044f25cfaa4d10437cc604f5fd28d69a27a86547
Vaishnavi <vaishdw2207@gmail.com>
2021-01-31 18:46:37 +0530

    feat: add sqflite

---
+++ lib/core/models/model.dart
@@ -0,0 +1,30 @@
+enum TodoStatus { active, done }
+
+class Todo {
+  int id;
+  String title;
+  DateTime created;
+  DateTime updated;
+  int status;
+
+  Todo({this.id, this.title, this.created, this.updated, this.status});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'created': created.toString(),
+      'updated': updated.toString(),
+      'status': status,
+    };
+  }
+
+  Map<String, dynamic> toMapAutoID() {
+    return {
+      'title': title,
+      'created': created.toString(),
+      'updated': updated.toString(),
+      'status': TodoStatus.active.index,
+    };
+  }
+}
